2677a2bc10c7db9208b2c6789818d5605d61de7d
remyf <remy.furnari@gmail.com>
2017-05-30 16:54:04 +0200

    header/footer

---
+++ footer.php
@@ -0,0 +1,32 @@
+<html><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+<footer>
+            <div class="mentionslegales">
+                <img src="images/avatar.svg" alt="avatar" class="avatar">
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Advertising &amp; Partnerships</a></li>
+                    <li><a href="#">How to Get Featured</a></li>
+                    <li><a href="#">Archive</a></li>
+                    <li><a href="#">Privacy</a></li>
+                </ul>
+                <p class="apropos">Creative Boom celebrates, inspires and supports the creative community.<br/> Launched in 2009, we love to explore the very best creativity and offer tips, <br/>resources and advice to help you succeed.
+                </p>
+                <p class="copyright">©2016 Creative Boom Ltd. Registered in England and Wales #07437294.</p>
+                <p class="creator">Created by Boomerang PR</p>
+            </div>
+        </footer>
+
+</body>
+</html>
