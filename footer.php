2ecef45a292f03d6a65854b5a86d88e7a4657e98
remyf <remy.furnari@gmail.com>
2017-05-31 10:06:08 +0200

    add main

--- footer.php
+++ footer.php
@@ -1,14 +1,3 @@
-<html><!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-
 
 <footer>
             <div class="mentionslegales">
@@ -27,6 +16,3 @@
                 <p class="creator">Created by Boomerang PR</p>
             </div>
         </footer>
-
-</body>
-</html>
--- site.php
+++ site.php
@@ -13,6 +13,215 @@
   include('header.php')
 
   ?>
+ <!--lead article-->
+        <section class="banniere">
+            <a href="http://www.creativeboom.com/inspiration/a-creatives-guide-to-new-york-city/">
+                <h2>A creative's guide to New York City</h2>
+                <p>19th April in <a href="#">Inspiration / Graphic Design</a></p>
+            </a>
+        </section>
+
+        <!--articles gallery-->
+        <section class="articlesgallery">
+            <article class="leadarticle">
+                <a href="#"><img src="images/newyork.jpeg" alt="bureau"></a>
+                <div="legende">
+                    <p>A creative's guide to New York City</p>
+                    <p class="date">19th April in <a href="#">Inspiration / Graphic Design</a> </p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article2.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Jia Liu's loveable, illustrated characters that inspire and delight children everywhere</p>
+                    <p class="date">19th April in <a href="#">Inspiration / Illustration</a> </p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article3.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Fleet Collective's factory-inspired identity for Dundee Design Festival is open source for you to play with
+                    </p>
+                    <p class="date"> 19th April in <a href="#">Inspiration / Graphic Design</a> </p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article4.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article5.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article1.png" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article6.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+        </section>
+
+        <!--editor's picks section-->
+        <section class="editor">
+            <h1>Editor’s Picks</h1>
+            <div class="editorspicks">
+                <article>
+                    <a href="#"><img src="images/editor1.jpeg" alt="bureau"></a>
+                    <div class="legende">
+                        <p>Save time and effort: speed up document sign-off with Eversign </p>
+                        <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+                </article>
+                <article>
+                    <a href="#"><img src="images/editor2.jpeg" alt="bureau"></a>
+                    <div class="legende">
+                        <p>Save time and effort: speed up document sign-off with Eversign </p>
+                        <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+                </article>
+            </div>
+        </section>
+
+
+        <!--banner ad 1-->
+        <aside>
+            <img src="images/adobe.jpg" alt="adobe">
+        </aside>
+
+
+        <!--articles gallery 2-->
+        <section class="articlesgallery">
+            <article>
+                <a href="#"><img src="images/article11.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article12.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>This Must Be The Place: Hannah Warren's illustrations of cycling in London</p>
+                    <p class="date"> 20th April in <a href="#">Inspiration / Illustration </a></p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article13.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article14.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article15.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article6.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+        </section>
+
+
+        <!--banner ad 2-->
+        <aside>
+            <img src="images/pub2.jpg" alt="pub">
+        </aside>
+
+
+        <!--articles gallery 3-->
+        <section class="articlesgallery">
+            <article>
+                <a href="#"><img src="images/article7.jpeg" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article8.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article9.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article10.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article1.png" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+            <article>
+                <a href="#"><img src="images/article6.jpg" alt="bureau"></a>
+                <div="legende">
+                    <p>Save time and effort: speed up document sign-off with Eversign </p>
+                    <p class="date"> 19th April in <a href="#">Resources / Tools</a></p>
+                    </div>
+            </article>
+        </section>
+
+        <!--load more button-->
+        <section class="loadmore">
+            <a href="#">
+                <p>Load more</p>
+            </a>
+        </section>
+
+
+        <!--newsletter-->
+        <section class="newsletter">
+            <div class="boxnewsletter">
+                <p>Get the best of Creative Boom <br/>delivered to your inbox weekly</p>
+                <form action="/ma-page-de-traitement" method="post">
+                    <input type="text" id="nom" placeholder="Your email..." />
+                    <button type="submit">Suscribe</button>
+                </form>
+            </div>
+        </section>
+
+
 
   <?php
 
